db6ee047032c247fa131fd856faf93485cc77296
MR <matthias.rohrmose@gmail.com>
2018-05-20 13:35:13 +0200

    Updated Webpack config; First working draft

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/typescript/test.ts',
+    mode : "development",
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                title: "Custom template",
+                template: 'src/index.html'
+            }
+        )
+    ],
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '_dist')
+    }
+};
