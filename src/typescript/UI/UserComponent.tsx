65bd9da6d03e9164847a608668556415eed053c0
MR <matthias.rohrmose@gmail.com>
2018-05-20 21:59:40 +0200

    Added first components

---
+++ src/typescript/UI/UserComponent.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import {Domain} from "../domain/User";
+import IUser = Domain.IUser;
+
+
+export default class UserComponent extends React.Component<IUser, {}> {
+
+    constructor (props: UserInterface){
+        super(props);
+    }
+    render() {
+        return (  <div>
+                <h1>User Component</h1>
+                Hello, {this.props.name}
+                <br/>
+                You are {this.props.age} years old,
+                <br/>
+                You live at: {this.props.address}
+                <br/>
+                you were born: {this.props.dob.toDateString()}
+            </div>
+
+        );
+    }
+}
---
+++ src/typescript/domain/User.ts
@@ -0,0 +1,8 @@
+export namespace Domain{
+    export interface IUser{
+        name: string;
+        age: number;
+        address: string;
+        dob: Date;
+    }
+}
