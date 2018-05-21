44f72427221f6f257a54858a8367267539c387fb
MR <matthias.rohrmose@gmail.com>
2018-05-21 08:25:01 +0200

    Updated app

--- src/typescript/UI/UserComponent.tsx
+++ src/typescript/UI/UserComponent.tsx
@@ -2,24 +2,22 @@ import * as React from 'react';
 
 import {IUser} from '../domain/User';
 
-export default class UserComponent extends React.Component<IUser, IUser, {}> {
+export default class UserComponent extends React.Component<IUser,{}> {
 
     constructor(props: IUser) {
         super(props);
     }
     public render() {
-        return (  <div>
-                <button onClick={e => this.props.handleClick()}>Check</button>
-                <h1>User Component</h1>
-                Hello, {this.props.name}
-                <br/>
-                You are {this.props.age} years old,
-                <br/>
-                You live at: {this.props.address}
-                <br/>
-                you were born: {this.props.dob.toDateString()}
-            </div>
-
-        );
+        return <div>
+            <button onClick={e => this.props.handleClick()}>Check</button>
+            <h1>User Component</h1>
+            Hello, {this.props.name}
+            <br/>
+            You are {this.props.name} years old,
+            <br/>
+            You live at: {this.props.address}
+            <br/>
+            you were born: {this.props.dob.toDateString()}
+        </div>;
     }
 }
