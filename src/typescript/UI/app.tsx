58eb73cefed36f8f275f206b6ac21e1da8c685dc
MR <matthias.rohrmose@gmail.com>
2018-11-17 09:09:10 +0100

    ???

--- src/typescript/UI/app.tsx
+++ src/typescript/UI/app.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import FirstComponent from './FirstComponent';
 
+const mainContentStyle = {
+  gridArea: 'mainContent',
+  backgroundColor: 'orange'
+};
+const menuStyle = {
+  gridArea: 'menu',
+  backgroundColor: 'green',
+};
+const rootStyle = {
+  display:'grid',
+  gridTemplateColumns: '20% auto',
+  gridTemplateRows: '20% auto 10%',
+  gridTemplateAreas: 'header header menu mainContent footer footer',
+};
+
+const headerStyle = {
+  gridArea: 'header',
+  backgroundColor: 'grey'
+};
+const footer = {
+  gridArea: 'footer'
+};
 ReactDOM.render(
-    <div>
-        <h1>Hello, Welcome to the first page</h1>
-        <FirstComponent/>
-    </div>,
+      <div style={rootStyle}>
+        <header style={headerStyle}>header!</header>
+        <div style={menuStyle}>
+          Left pane
+        </div>
+        <main style={mainContentStyle}>
+          main content
+        </main>
+        <footer style={footer}>FOOTER!</footer>
+      </div>,
 document.getElementById('root'),
 );
