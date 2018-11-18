f079e18f9dba18a226703c65240b8eccfa3db906
MR <matthias.rohrmoser@gmail.com>
2018-11-18 10:07:43 +0100

    Moved and improved style

--- src/app.tsx
+++ src/app.tsx
@@ -1,40 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { IUser, User } from './typescript/domain/User';
-import UserComponent from './typescript/UI/UserComponent';
 
 import './app.css';
-
-const mainContentStyle = {
-  gridArea: 'mainContent',
-  backgroundColor: 'orange'
-};
-const menuStyle = {
-  gridArea: 'menu',
-  backgroundColor: 'green',
-};
-
-
-const headerStyle = {
-  gridArea: 'header',
-  backgroundColor: 'grey'
-};
-const footer = {
-  gridArea: 'footer'
-};
-
-const user: IUser = new User('YOLO', 1234);
+import CurrentWorkItem from './components/currentWorkItem/CurrentWorkItem';
 
 ReactDOM.render(
   <div className="root">
-    <header style={headerStyle}>header!</header>
-    <div style={menuStyle}>
+    <header className='header'>header!</header>
+    <div className='menu'>
       Left pane
         </div>
-    <main style={mainContentStyle}>
-      <UserComponent {...user} ></UserComponent>
+    <main className="mainContent">
+      <CurrentWorkItem></CurrentWorkItem>
     </main>
-    <footer style={footer}>FOOTER!</footer>
+    <footer className='footer'>FOOTER!</footer>
   </div>,
   document.getElementById('root'),
 );
