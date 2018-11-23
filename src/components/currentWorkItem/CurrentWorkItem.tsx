12a0a033c18f5bd77ea59b21e0909afe70d781e5
MR <matthias.rohrmoser@gmail.com>
2018-11-23 18:58:44 +0100

    Fixed error

--- src/components/currentWorkItem/CurrentWorkItem.tsx
+++ src/components/currentWorkItem/CurrentWorkItem.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { CurrentWorkItemService, serviceInstance } from './CurrentWorkItemService';
+import { serviceInstance } from './CurrentWorkItemService';
 
 export default class CurrentWorkItem extends React.Component {
-  public service: CurrentWorkItemService;
 
   public render() {
     return <div>
-      <span>Current Item: </span> <input type='text' onInput={e => this.service.GetWorkItems(e.currentTarget.value)}></input>
+      <span>Current Item: </span> <input type='text' onInput={e => serviceInstance.GetWorkItems(e.currentTarget.value)}></input>
     </div >;
   }
 }
