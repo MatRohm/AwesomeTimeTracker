6e39636805f4c046cd33271b19e2835d01325a13
MR <matthias.rohrmoser@gmail.com>
2018-11-23 18:12:47 +0100

    Improved webpack config

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,26 +1,12 @@
-const path = require('path');
-const createElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
 const merge = require('webpack-merge');
 const clientBaseConfig = require('./base.webpack.config');
 
-// Create one plugin for both renderer and main process
-const ElectronReloadWebpackPlugin = createElectronReloadWebpackPlugin({
-  // Other 'electron-connect' options
-  path: path.resolve(__dirname, '../src/main.js'),
-  logLevel: 0,
-});
-
 const devConfig = {
   devtool: 'source-maps',
-  watch: true,
+  output: {
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+  },
 };
 
-const electonConfig = {
-  target: 'electron-renderer',
-  plugins: [
-    ElectronReloadWebpackPlugin(),
-  ],
-};
-
-const webpackConfig = merge(clientBaseConfig, devConfig, electonConfig);
-module.exports = [webpackConfig];
+const webpackConfig = merge(clientBaseConfig, devConfig);
+module.exports = webpackConfig;
