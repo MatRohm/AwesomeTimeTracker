f950386b001f33862249985e7d61fadf97b1d32a
MR <matthias.rohrmoser@gmail.com>
2018-12-14 23:19:31 +0100

    Single allowed-no any

--- src/Shared/Arguments/ArgumentUtility.ts
+++ src/Shared/Arguments/ArgumentUtility.ts
@@ -1,6 +1,7 @@
 import { ArgumentNullError } from './ArgumentNullError';
 
 export class ArgumentUtility {
+  // tslint:disable-next-line:no-any
   public static CheckDefined(argumentName: string, argumentValue: any): void {
     if (argumentValue == null || argumentValue === undefined) {
       throw new ArgumentNullError(argumentName);
