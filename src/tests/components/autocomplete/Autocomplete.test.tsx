0e4f61e21ab924ea119a9b31e2b82aa6aea8bfb6
MR <matthias.rohrmoser@gmail.com>
2018-12-14 21:46:27 +0100

    Improved tests

--- src/tests/components/autocomplete/Autocomplete.test.tsx
+++ src/tests/components/autocomplete/Autocomplete.test.tsx
@@ -1,4 +1,4 @@
-import { mount, shallow, ShallowWrapper, render } from 'enzyme';
+import { mount } from 'enzyme';
 import Autocomplete from '../../../components/autocomplete/Autocomplete';
 import * as React from 'react';
 import IAutocompleteDataSource from '../../../components/autocomplete/IAutocompleteDataSource';
@@ -23,7 +23,10 @@ describe('Test if autocomplete renders correctly', () => {
     const mock = TypeMoq.Mock.ofType<IAutocompleteDataSource>();
 
     mock.setup(o => o.GetEntries('test')).returns(() => {
-      return [new AutoCompleteEntry('1', 'test')];
+      return [
+        new AutoCompleteEntry('1', 'test'),
+        new AutoCompleteEntry('2', 'second test')
+      ];
     });
 
     mock.setup(o => o.GetEntries('t')).returns(() => {
@@ -33,42 +36,60 @@ describe('Test if autocomplete renders correctly', () => {
     return mock;
   }
   it('When no text is given no entries are shown', () => {
+    // Arrange
     const ac = getOrCreateAutocomplete();
+
+    // Act
     const countOfListEntries = ac.find('ul > li').length;
+
+    // Assert
     expect(countOfListEntries).toBe(0);
   });
 
   it('When a text is given entries are shown', () => {
+    // Arrange
     const autocomplete = getOrCreateAutocomplete();
-
-    const simulateEventArgs = { target: { value: 'test' } };
     const inputField = autocomplete.find('input');
-    inputField.value = 'IRGENDWAS!!!';
+
+    // Act
+    const simulateEventArgs = { target: { value: 'test' } };
     inputField.simulate('change', simulateEventArgs);
 
+    // Assert
     const countOfListEntries = autocomplete.find('ul > li').length;
-    expect(countOfListEntries).toBe(1);
+    expect(countOfListEntries).toBe(2);
   });
 
   it('When a text is given then removed, no entries are shown', () => {
+    // Arrange
     const ac = getOrCreateAutocomplete();
+
+    // Act
     const simulateEventArgs = { target: { value: 'test' } };
     ac.find('input').simulate('change', simulateEventArgs);
 
     simulateEventArgs.target.value = '';
     ac.find('input').simulate('change', simulateEventArgs);
 
+    // Assert
     const countOfListEntries = ac.find('ul > li').length;
     expect(countOfListEntries).toBe(0);
   });
 
-  it('When an entry is clicked its text is written to the input box', () => {
+  it('When an entry is clicked its text is written to the input box and the entries are removed', () => {
+    // Arrange
     const ac = getOrCreateAutocomplete();
     const simulateEventArgs = { target: { value: 't' } };
     ac.find('input').simulate('change', simulateEventArgs);
 
+    // Act
     ac.find('ul > li').simulate('click');
+
+    // Assert
     const textOfInput = ac.find('input').props().value;
     expect(textOfInput).toBe('test');
+
+    const numberOfEntries = ac.find('ul > li').length;
+    expect(numberOfEntries).toBe(0);
   });
 });
