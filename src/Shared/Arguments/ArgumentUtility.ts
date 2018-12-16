69e9e02b837982c9a4b45bc293c151f4f74026cb
MR <matthias.rohrmoser@gmail.com>
2018-12-16 12:02:54 +0100

    Issue #1 - Basisstufe Timetracking

--- src/Shared/Arguments/ArgumentUtility.ts
+++ src/Shared/Arguments/ArgumentUtility.ts
@@ -1,6 +1,10 @@
 import { ArgumentNullError } from './ArgumentNullError';
 
 export class ArgumentUtility {
+
+  /**
+   * Checks if argument is defined (not null and not undefined), if not an error is thrown
+   */
   // tslint:disable-next-line:no-any
   public static CheckDefined(argumentName: string, argumentValue: any): void {
     if (argumentValue == null || argumentValue === undefined) {
