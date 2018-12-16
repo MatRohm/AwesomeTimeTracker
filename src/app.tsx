3ebdac45f9c583b29240a816c6a3ae0fb760157e
MR <matthias.rohrmoser@gmail.com>
2018-12-16 10:23:14 +0100

    Issue #1 - Basisstufe Timetracking

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import './app.css';
-import CurrentWorkitem from './components/currentWorkItem/CurrentWorkItem';
+import CurrentWorkitem from './components/currentWorkItem/CurrentWorkitem';
 
 ReactDOM.render(
   <div className='root'>
