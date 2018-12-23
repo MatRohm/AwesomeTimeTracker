c5c6b33928a64a5528140350641daa0e583b3074
MR <matthias.rohrmoser@gmail.com>
2018-12-23 11:14:58 +0100

    Moved blocks into correct scopes

--- src/tests/shared/arguments/ArgumentUtility.test.ts
+++ src/tests/shared/arguments/ArgumentUtility.test.ts
@@ -30,7 +30,7 @@ describe('Test whether ArgumentUtility.checkDefinedAndObjectLiteral works correc
 
   it('When given a function it throws an error', () => {
     // tslint:disable-next-line:only-arrow-functions no-empty
-    const value = function () { };
+    const value = function() { };
     expect(() => ArgumentUtility.checkDefinedAndObjectLiteral('value', value)).toThrow();
   });
 
@@ -65,91 +65,51 @@ describe('Test whether ArgumentUtility.checkDefinedAndObjectLiteral works correc
     const value = new sampleClass();
     expect(() => ArgumentUtility.checkDefinedAndObjectLiteral('value', value)).toThrow();
   });
+});
 
-  describe('Tests whether ArgumentUtility.checkHasContent() works correctly', () => {
-    it('When given null it throws an error', () => {
-      const value: string = null;
-      expect(() => ArgumentUtility.checkHasContent('value', value)).toThrow();
-    });
+describe('Tests whether ArgumentUtility.checkHasContent() works correctly', () => {
+  it('When given null it throws an error', () => {
+    const value: string = null;
+    expect(() => ArgumentUtility.checkHasContent('value', value)).toThrow();
+  });
 
-    it('When given undefined it throws an error', () => {
-      expect(() => ArgumentUtility.checkHasContent('value', undefined)).toThrow();
-    });
+  it('When given undefined it throws an error', () => {
+    expect(() => ArgumentUtility.checkHasContent('value', undefined)).toThrow();
+  });
 
-    it('When given a string with whitespace only it throws an error', () => {
-      const value = ' ';
-      expect(() => ArgumentUtility.checkHasContent('value', value)).toThrow();
-    });
+  it('When given a string with whitespace only it throws an error', () => {
+    const value = ' ';
+    expect(() => ArgumentUtility.checkHasContent('value', value)).toThrow();
+  });
 
-    it('When given a string value it does not throw an error', () => {
-      const value = 'this is a string';
-      expect(() => ArgumentUtility.checkHasContent('value', value)).not.toThrow();
-    });
+  it('When given a string value it does not throw an error', () => {
+    const value = 'this is a string';
+    expect(() => ArgumentUtility.checkHasContent('value', value)).not.toThrow();
+  });
+});
 
-    describe('Tests whether ArgumentUtility.checkDefinedAndFunction() works correctly', () => {
-      it('When given a string it throws an error', () => {
-        const value = 'this is a string';
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).toThrow();
-      });
+describe('Tests whether ArgumentUtility.checkHasElements() works correctly', () => {
+  it('When given an empty array it throws an error', () => {
+    const value = new Array<number>();
+    expect(() => ArgumentUtility.checkHasElements('value', value)).toThrow();
+  });
 
-      it('When given null it throws an error', () => {
-        // tslint:disable-next-line:no-any
-        const value: any = null;
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).toThrow();
-      });
+  it('When given null it throws an error', () => {
+    const value: [] = null;
+    expect(() => ArgumentUtility.checkHasElements('value', value)).toThrow();
+  });
 
-      it('When given undefined it throws an error', () => {
-        // tslint:disable-next-line:no-any
-        const value: any = undefined;
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).toThrow();
-      });
+  it('When given undefined it throws an error', () => {
+    expect(() => ArgumentUtility.checkHasElements('value', undefined)).toThrow();
+  });
 
-      it('When given an arrow function it does not throw an error', () => {
-        const value = () => { };
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).not.toThrow();
-      });
+  it('When given an array with one member it does not throw an error', () => {
+    const value = [1];
+    expect(() => ArgumentUtility.checkHasElements('value', value)).not.toThrow();
+  });
 
-      it('When given an function it does not throw an error', () => {
-        // tslint:disable-next-line:only-arrow-functions
-        const value = function () { };
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).not.toThrow();
-      });
-
-      it('When given an object it throws an error', () => {
-        const value = {};
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).toThrow();
-      });
-
-      it('When given a number it throws an error', () => {
-        const value = 123;
-        expect(() => ArgumentUtility.checkDefinedAndFunction('value', value)).toThrow();
-      });
-    });
-
-    describe('Tests whether ArgumentUtility.checkHasElements() works correctly', () => {
-      it('When given an empty array it throws an error', () => {
-        const value = new Array<number>();
-        expect(() => ArgumentUtility.checkHasElements('value', value)).toThrow();
-      });
-
-      it('When given null it throws an error', () => {
-        const value: [] = null;
-        expect(() => ArgumentUtility.checkHasElements('value', value)).toThrow();
-      });
-
-      it('When given undefined it throws an error', () => {
-        expect(() => ArgumentUtility.checkHasElements('value', undefined)).toThrow();
-      });
-
-      it('When given an array with one member it does not throw an error', () => {
-        const value = [1];
-        expect(() => ArgumentUtility.checkHasElements('value', value)).not.toThrow();
-      });
-
-      it('When given an array with two strings it does not throw an error', () => {
-        const value = ['Wall', 'Mart'];
-        expect(() => ArgumentUtility.checkHasElements('value', value)).not.toThrow();
-      });
-    });
+  it('When given an array with two strings it does not throw an error', () => {
+    const value = ['Wall', 'Mart'];
+    expect(() => ArgumentUtility.checkHasElements('value', value)).not.toThrow();
   });
 });
