581c8e0aaf55659f2a94f8416cb7f9176c27bb38
MR <matthias.rohrmoser@gmail.com>
2019-02-02 21:42:09 +0100

    Remove console.log

--- src/components/currentWorkitem/CurrentWorkItem.tsx
+++ src/components/currentWorkitem/CurrentWorkItem.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import Autocomplete from '../autocomplete/Autocomplete';
 import { Shoes } from './Shoes';
-import IAutocompleteDataSource from '../autocomplete/IAutocompleteDataSource';
 
 export default class CurrentWorkitem extends React.Component {
 
   public render() {
-
-    const autoCompleteDataSource: IAutocompleteDataSource = new Shoes();
-    console.log('autoCompleteDataSource', autoCompleteDataSource);
-
     return <div>
       <Autocomplete dataSource={new Shoes()} />
     </div >;
