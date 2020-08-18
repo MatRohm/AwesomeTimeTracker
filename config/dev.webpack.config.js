9e7f91d94777c73c7fb0e55f14f7fba696dbe29c
Matthias Rohrmoser <matthis.rohrmoser@gmail.com>
2020-08-18 21:00:36 +0200

    Simplified Build

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,12 +1,27 @@
 const merge = require('webpack-merge');
 const clientBaseConfig = require('./base.webpack.config');
+const createElectronReloadWebpackPlugin = require('electron-reload-webpack-plugin');
+const path = require('path');
+
+
+// Create one plugin for both renderer and main process
+const ElectronReloadWebpackPlugin = createElectronReloadWebpackPlugin({
+  // Other 'electron-connect' options
+  path: path.resolve(__dirname, '../src/main.js'),
+  logLevel: 0,
+});
 
 const devConfig = {
   devtool: 'source-maps',
   output: {
     devtoolModuleFilenameTemplate: '[resource-path]',
   },
+  watch: true,
+  target: 'electron-renderer',
+  plugins: [
+    ElectronReloadWebpackPlugin(),
+  ],
 };
 
 const webpackConfig = merge(clientBaseConfig, devConfig);
-module.exports = webpackConfig;
+module.exports = [clientBaseConfig, webpackConfig];
