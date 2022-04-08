e1556772b6db86d18c3db9df0fe45215804e7f22
Matthias Rohrmoser <matthias.rohrmoser@gmail.com>
2022-04-08 15:42:29 +0200

    Enabled hot reloading

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ const path = require('path');
 const url = require('url');
 
 // Enable live reload for Electron too
-require('electron-reload')(path.resolve(__dirname, './_dist'));
+require('electron-reload')(".");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
