e6b6215dddeb78ce51bb15b1083472366875defa
Matthias Rohrmoser <matthias.rohrmoser@gmail.com>
2022-04-08 19:57:41 +0200

    Fixed linting errors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // This is the entry point for the electron app start
 const electron = require('electron');
-const sourceMapSupport = require('source-map-support');
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
@@ -10,38 +9,38 @@ const path = require('path');
 const url = require('url');
 
 // Enable live reload for Electron too
-require('electron-reload')(".");
+require('electron-reload')('.');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 1300,
-        height: 800,
-        icon: path.resolve(__dirname, '../_dist/icon.ico'),
-    });
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1300,
+    height: 800,
+    icon: path.resolve(__dirname, '../_dist/icon.ico'),
+  });
 
-    mainWindow.setMenu(null);
-    // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '../_dist/index.html'),
-        protocol: 'file:',
-        slashes: true,
-    }));
+  mainWindow.setMenu(null);
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, '../_dist/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -50,18 +49,18 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -11,47 +11,47 @@ const distDir = path.resolve(__dirname, './_dist/');
 // either inject file via file-loader with style-loder/url
 // OR inject css directly via css-loader with style-loader in html dom
 const clientBaseConfig = {
-    entry: [path.resolve(srcDir, 'app.tsx')],
-    target: 'web',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                use: 'ts-loader',
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use:[MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                loader: 'file-loader',
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'fonts/',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin(
-            {
-                title: 'Custom template',
-                template: path.resolve(srcDir, 'index.html'),
-            },
-        ),
-        new CopyWebpackPlugin({ patterns: [{ from: path.resolve(srcDir, 'assets/icon.ico'), to: distDir }] }),
+  entry: [path.resolve(srcDir, 'app.tsx')],
+  target: 'web',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        loader: 'file-loader',
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
+        },
+      },
     ],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: distDir,
-    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin(
+      {
+        title: 'Custom template',
+        template: path.resolve(srcDir, 'index.html'),
+      }
+    ),
+    new CopyWebpackPlugin({ patterns: [{ from: path.resolve(srcDir, 'assets/icon.ico'), to: distDir }] }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: distDir,
+  },
 };
 
 module.exports = clientBaseConfig;
